6b25f1fcc94f26116d98092824d1b9f14de00189
Cam <camhack67@gmail.com>
2023-08-28 18:45:55 -0400

    added choices

--- index.mjs
+++ index.mjs
@@ -1,18 +1,19 @@
 import inquirer from 'inquirer';
 import mysql from "mysql2";
 
-// Connect to database
+// Connect to employee_db database
 const db = mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: '',
       database: 'employee_db'
-});
+})
 
 // starts the program
 runProgram()
 async function runProgram() {
     try {
+        // initial prompt to navigate this program
         let answers = await inquirer
         .prompt([
             {
@@ -31,7 +32,7 @@ async function runProgram() {
             }
         ])
 
-        // allows the user to keep using the program after pressing enter
+        // user friendly option to press enter after every path ends returns user to the initial prompt
         async function pressEnterToContinue() {
             await inquirer.prompt([
                 {
@@ -40,15 +41,17 @@ async function runProgram() {
                 message: '\x1b[35mPress Enter to continue\x1b[0m',
                 },
             ])
-            
+
             runProgram()
         }
-
-        // view tables
-        async function viewDepartments() {
-            let viewDepartments = `SELECT name, id FROM department;`
         
-            db.query(viewDepartments, function (err, data) {
+        // database table selections used often
+        const viewDepartmentTable = `SELECT name, id FROM department;`
+        const viewRoleTable = `SELECT title as JobTitle, role.id as RoleID, department.name as DepartmentName, salary FROM role INNER JOIN department ON role.department_id = department.id;`
+        const viewEmployeeTable = `SELECT employee.id AS EmployeeID, employee.first_name, employee.last_name, role.title AS JobTitle, department.name AS Department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id;`
+
+        async function viewDepartments() {
+            db.query(viewDepartmentTable, (err, data) => {
                 if (err) {
                     console.log(err)
                 } else {
@@ -59,9 +62,7 @@ async function runProgram() {
         }
 
         async function viewRoles() {
-            let viewRoles = `SELECT title as JobTitle, role.id as RoleID, department.name as DepartmentName, salary FROM role INNER JOIN department ON role.department_id = department.id;`
-        
-            db.query(viewRoles, function (err, data) {
+            db.query(viewRoleTable, (err, data) => {
                 if (err) {
                     console.log(err)
                 } else {
@@ -71,10 +72,8 @@ async function runProgram() {
             })
         }
 
-        async function viewEmployees() {
-            let viewEmployees = `SELECT employee.id AS EmployeeID, employee.first_name, employee.last_name, role.title AS JobTitle, department.name AS Department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id;`
-        
-            db.query(viewEmployees, function (err, data) {
+        async function viewEmployees() {        
+            db.query(viewEmployeeTable, (err, data) => {
                 if (err) {
                     console.log(err)
                 } else {
@@ -108,10 +107,12 @@ async function runProgram() {
                         }
                     ])
 
-                    db.query(`INSERT INTO department (name) VALUES (?);`, [departmentInfo.name], function (err, data) {
+                    // adds a new department based on above inquirer input
+                    db.query(`INSERT INTO department (name) VALUES (?);`, [departmentInfo.name], (err) => {
                         if (err) {
                             console.log(err);
                         } else {
+                            // confirmation that department was added
                             console.log(`\x1b[34mDepartment \x1b[33m'${departmentInfo.name}'\x1b[34m has been added.\x1b[0m`)
                             pressEnterToContinue()
                         }
@@ -129,6 +130,14 @@ async function runProgram() {
         if (answers.menuChoice === 'Add a role') {
             async function addRole() {
                 try {
+                    const departmentChoices = await new Promise((res) => {
+                        // grabs list of departments from the department table
+                        db.query(viewDepartmentTable, (err, data) => {
+                            // converts department data from the above query into a format accepted by inquirer choices
+                            res(data.map((data) => ({name: data.name, value: data.id})))
+                        })
+                    })
+
                     let roleInfo = await inquirer
                     .prompt([
                         {
@@ -142,16 +151,19 @@ async function runProgram() {
                             message: 'Enter the salary of this role:',
                         },
                         {
-                            type: 'input',
+                            type: 'list',
                             name: 'department_id',
-                            message: 'Enter the department ID that this role belongs to:',
+                            message: 'Choose the department for this role:',
+                            choices: departmentChoices,
                         }
                     ])
 
-                    db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);`, [roleInfo.title, roleInfo.salary, roleInfo.department_id], function (err, data) {
+                    // adds a new role based on above inquirer input
+                    db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);`, [roleInfo.title, roleInfo.salary, roleInfo.department_id], (err) => {
                         if (err) {
                             console.log(err);
                         } else {
+                            // confirmation that the new role was added
                             console.log(`\x1b[34mRole \x1b[33m'${roleInfo.title}'\x1b[34m has been added.\x1b[0m`)
                             pressEnterToContinue()
                         }
@@ -169,6 +181,22 @@ async function runProgram() {
         if (answers.menuChoice === 'Add an employee') {
             async function addEmployee() {
                 try {
+                    const roleChoices = await new Promise((res) => {
+                        // grabs list of roles from the role table
+                        db.query(viewRoleTable, (err, data) => {
+                            // converts role data from the above query into a format accepted by inquirer choices
+                            res(data.map((data) => ({name: data.JobTitle, value: data.RoleID})))
+                        })
+                    })
+
+                    const managerChoices = await new Promise((res) => {
+                        // grabs list of employees from the employee table
+                        db.query(viewEmployeeTable, (err, data) => {
+                            // converts employee data from the above query into a format accepted by inquirer choices
+                            res(data.map((data) => ({name: `${data.first_name} ${data.last_name}`, value: data.EmployeeID})))
+                        })
+                    })
+
                     let employeeInfo = await inquirer
                     .prompt([
                         {
@@ -182,21 +210,26 @@ async function runProgram() {
                             message: 'Enter the Last Name of the employee:',
                         },
                         {
-                            type: 'input',
+                            type: 'list',
                             name: 'role_id',
-                            message: 'Enter the Role ID of this employee:',
+                            message: 'Choose the Role for this employee:',
+                            choices: roleChoices,
                         },
                         {
-                            type: 'input',
+                            type: 'list',
                             name: 'manager_id',
-                            message: 'Enter the Manager ID of this employee (leave blank if this person is the manager):',
+                            message: 'Choose the Manager of this employee:',
+                            // Create a new option called "None" before all employees to choose to be managers
+                            choices: [{name: "None", value: null}, ...managerChoices],
                         }
                     ])
 
-                    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`, [employeeInfo.first_name, employeeInfo.last_name, employeeInfo.role_id, employeeInfo.manager_id], function (err, data) {
+                    // adds a new employee based on above inquirer input
+                    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`, [employeeInfo.first_name, employeeInfo.last_name, employeeInfo.role_id, employeeInfo.manager_id], (err) => {
                         if (err) {
                             console.log(err);
                         } else {
+                            // confirmation that the new employee was added
                             console.log(`\x1b[34mEmployee \x1b[33m'${employeeInfo.first_name}'\x1b[34m has been added.\x1b[0m`)
                             pressEnterToContinue()
                         }
@@ -214,24 +247,43 @@ async function runProgram() {
         if (answers.menuChoice === 'Update an employee role') {
             async function updateEmployeeRole() {
                 try {
+                    const employeeChoices = await new Promise((res) => {
+                        // grabs list of employees from the employee table
+                        db.query(viewEmployeeTable, (err, data) => {
+                            // converts employee data from the above query into a format accepted by inquirer choices
+                            res(data.map((data) => ({name: `${data.first_name} ${data.last_name}`, value: data.EmployeeID})))
+                        })
+                    })
+                    const roleChoices = await new Promise((res) => {
+                        // grabs list of roles from the role table
+                        db.query(viewRoleTable, (err, data) => {
+                            // converts role data from the above query into a format accepted by inquirer choices
+                            res(data.map((data) => ({name: data.JobTitle, value: data.RoleID})))
+                        })
+                    })
+
                     let updateRole = await inquirer
                     .prompt([
                         {
-                            type: 'input',
+                            type: 'list',
                             name: 'employee_id',
                             message: 'Enter the Employee ID of the person whose Role you want to update:',
+                            choices: employeeChoices,
                         },
                         {
-                            type: 'input',
+                            type: 'list',
                             name: 'role_id',
                             message: 'Enter the new Role ID for this employee:',
+                            choices: roleChoices,
                         }
                     ])
 
-                    db.query(`UPDATE employee SET role_id = ? WHERE id = ?;`, [updateRole.role_id, updateRole.employee_id], function (err, data) {
+                    // updates employees role based on above inquirer input
+                    db.query(`UPDATE employee SET role_id = ? WHERE id = ?;`, [updateRole.role_id, updateRole.employee_id], (err) => {
                         if (err) {
                             console.log(err);
                         } else {
+                            // confirmation that employee has been updated a new role
                             console.log(`\x1b[34mEmployee role has been updated.\x1b[0m`)
                             pressEnterToContinue()
                         }
